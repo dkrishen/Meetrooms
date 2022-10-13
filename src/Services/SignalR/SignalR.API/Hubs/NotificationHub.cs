806157da72bc1695555f2869da83a553a23406fd
dkrishen <krishen@tut.by>
2022-10-13 11:00:28 +0300

    Added and initially configured project for signalR hub

---
+++ src/Services/SignalR/SignalR.API/Hubs/NotificationHub.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+ using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR.API.Hubs
+{
+    public class NotificationHub : Hub<INotificationHub>
+    {
+        private ChatManager chatManager;
+        private const string defaultGroupName = "General";
+
+        public NotificationHub(ChatManager chatManager)
+        {
+            this.chatManager = chatManager;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var tmp = Context.ConnectionId;
+
+            var userName = Context.User?.Identity?.Name ?? "Anonymous";
+            var connectionId = Context.ConnectionId;
+            chatManager.ConnectUser(userName, connectionId);
+
+            await Groups.AddToGroupAsync(connectionId, defaultGroupName);
+            await UpdateUsersAsync();
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var isUserRemoved = chatManager.DisconnectUser(Context.ConnectionId);
+            if (!isUserRemoved)
+            {
+                await base.OnDisconnectedAsync(exception);
+            }
+
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, defaultGroupName);
+            await UpdateUsersAsync();
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task UpdateUsersAsync()
+        {
+            var users = chatManager.Users.Select(x => x.UserName).ToList();
+            await Clients.All.UpdateUsersAsync(users);
+        }
+
+        public async Task SendNotificationAsync(string userName, string message)
+        {
+            await Clients.Others.SendNotificationAsync(userName, message);
+            //await Clients.User(userName).SendNotificationAsync(userName, message);
+        }
+    }
+}
